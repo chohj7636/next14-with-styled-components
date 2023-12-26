7794ce3a355a951bade1ace5b1c376575f2630d0
Owen <chohj7636@gmail.com>
2023-12-26 17:27:03 +0900

    보다 이미지 추가

---
+++ static/script.js
@@ -0,0 +1,21 @@
+(function (a, i, v, o, r, y) {
+  a.__aivoryClient__ = {
+    load: function (src) {
+      var b = i.createElement('script');
+      b.src = src;
+      b.async = true;
+      b.type = 'text/javascript';
+      i.getElementsByTagName('head')[0].appendChild(b);
+    },
+    ten: o,
+    rootDomain: r,
+  };
+  a.__aivoryClient__.load(v);
+})(
+  window,
+  document,
+  '//cdn.nerdfactory.ai/voda/agent/vodaintro/voda-agent.js',
+  'zw9Nt7EuIwo6t7mE',
+  '',
+  ''
+);
