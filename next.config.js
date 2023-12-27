051f6909742637816922dcbf3531e6bb41476579
Owen <chohj7636@gmail.com>
2023-12-27 09:13:10 +0900

    next에서 svg 사용할 수 있도록 세팅

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: 'export',
   trailingSlash: true,
   compiler: {
     styledComponents: true,
+  },
+  webpack: (config) => {
+    // 아래를 추가합니다.
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
   },
 };
 
